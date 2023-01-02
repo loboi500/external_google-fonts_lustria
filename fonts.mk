f3004941dab0d89d3efda56bdecd0032b570812a
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:06:09 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,17 +1,3 @@
-
-# Copyright (C) 2020 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-PRODUCT_PACKAGES := \
-	Lustria-Regular.ttf
+version https://git-lfs.github.com/spec/v1
+oid sha256:db2c45f0592d0e92dc8b980b69178bb346b6a20f916a01cc617a1a0db2e2ccd8
+size 644
